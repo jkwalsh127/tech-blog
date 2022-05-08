5ccaa9746b6dcdb3228ff86bc8eb1a33759868a4
Jake Walsh <jkwalsh127@gmail.com>
2022-05-08 09:42:30 -0700

    itiate comment model, route, and seed data

--- models/index.js
+++ models/index.js
@@ -1,8 +1,17 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
     onDelete: 'CASCADE'
 });
 
@@ -10,4 +19,12 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'comment_id'
+});
+
+module.exports = { User, Post, Comment };
