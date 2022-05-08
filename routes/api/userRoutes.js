ca1218dbce288bf40a06d54a7aae57a89c744663
Jake Walsh <jkwalsh127@gmail.com>
2022-05-07 21:27:49 -0700

    initiate new user post route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -11,4 +11,14 @@ router.get('/', async (req, res) => {
     }
   });
 
+// POST create a new user
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
