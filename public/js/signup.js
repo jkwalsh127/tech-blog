7d31c5dbd4d5502e74cce7874b7b6a878c509694
Jake Walsh <jkwalsh127@gmail.com>
2022-05-09 21:51:18 -0700

    fix bugs in signup form

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,18 +1,22 @@
 const signupUsername = document.querySelector('#username-signup');
 const signupPassword = document.querySelector('#password-signup');
-const signupForm = document.querySelector('#signup-form')
+const signupForm = document.querySelector('#signup-form');
 
 async function signupFormHandler(event) {
   event.preventDefault();
   const response = await fetch('/api/users', {
     method: 'POST',
-    body: JSON.stringify({ username: signupUsername.value.trim(), password: signupPassword.value.trim() }),
+    body: JSON.stringify({ 
+      username: signupUsername.value.trim(), 
+      password: signupPassword.value.trim(), 
+    }),
     headers: { 'Content-Type': 'application/json' },
   });
   if (response.ok) {
     document.location.replace('/dashboard');
   } else {
-    alert(response.statusText);
+    //this is not the appropriate place for such an alert, but it fits the functioning of the app at this time
+    alert('enter a password of 8 characters or longer');
   }
 };
 
